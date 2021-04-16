c23cfdcf758b38479d6f999c3304ae024465d67a
fatmameddeb <fatmameddeb1@esprit.tn>
2021-04-15 20:48:46 -1200

    commitfatma

---
+++ src/main/java/tn/esprit/spring/service/IStockSerivce.java
@@ -0,0 +1,20 @@
+package tn.esprit.spring.service;
+
+import java.util.List;
+
+import tn.esprit.spring.entities.Stock;
+
+public interface IStockSerivce {
+	
+	public int ajouterStock(Stock stock);
+	public void deleteStock(int IdStock);
+	public void UpdateStock(int IdStock,String NameStock, int QuantityStock);
+	public List<Stock> ShowStock();
+	public List<Stock> ShowStockbyName(String NameStock);
+	public List<Stock> ShowStockbyQuantity(int QuantityStock);
+
+	
+	
+	
+
+}
